8cb11534e194a4a777cf02f2cff0fc0777651dbe
Дмитрий Вершинин <wedmal94@gmail.com>
2020-01-31 17:10:53 +0700

    serverSocket

--- Broadcast/UPDProvider001.cs
+++ Broadcast/UPDProvider001.cs
@@ -26,18 +26,58 @@ namespace generateContentForInstructionSimonov.Broadcast
 
         public static void GetData() 
         {
-            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            IPEndPoint iep = new IPEndPoint(IPAddress.Any, 9050);
-            sock.Bind(iep);
-            EndPoint ep = (EndPoint)iep;
-            Console.WriteLine("Ready to receive...");
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.WorkerSupportsCancellation = false;
+            worker.WorkerReportsProgress = false;
 
-            byte[] data = new byte[1024];
-            int recv = sock.ReceiveFrom(data, ref ep);
-            string stringData = Encoding.ASCII.GetString(data, 0, recv);
-            Console.WriteLine("received: {0} ", stringData);
+            worker.DoWork += (_s, _e) =>
+            {
+                start_server_async();
+            };
 
-            sock.Close();
+            worker.RunWorkerCompleted += (_s, _e) =>
+            {
+                //close window
+            };
+
+            worker.RunWorkerAsync();
+
+
+            void start_server_async()
+            {
+                Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                IPEndPoint iep = new IPEndPoint(IPAddress.Any, 9050);
+                sock.Bind(iep);
+                EndPoint ep = (EndPoint)iep;
+                Console.WriteLine("Ready to receive...");
+                int convertByteToInt(byte[] b)
+                {
+                    int value = 0;
+                    for (int i = 0; i < b.Length; i++)
+                        value = (value << 8) | b[i];
+                    return value;
+                }
+
+                int ArrayLength = 1024;
+                while (true)
+                {
+                    byte[] data = new byte[16880];
+                    int recv = sock.ReceiveFrom(data, ref ep);
+                    int tmpInt = 0;
+                    try { tmpInt = convertByteToInt(new byte[] { data[0], data[1], data[2], data[3] }); } catch (Exception ex) { tmpInt = 1024; }
+                    if (tmpInt > 4)
+                    {
+                        ArrayLength = tmpInt;
+                    }
+                    string stringData = Encoding.ASCII.GetString(data, 0, recv);
+                    NewContentEvent?.Invoke(data, null);
+                    if (stringData.Contains("break"))
+                    {
+                        sock.Close();
+                    }
+                }
+            }
+
         }
 
     }
