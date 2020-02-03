9a6bf5a08bc788c5793742db0a115d07ffb9b53a
Дмитрий Вершинин <wedmal94@gmail.com>
2020-02-03 16:44:31 +0700

    stream video server.

--- Program.cs
+++ Program.cs
@@ -13,7 +13,8 @@ namespace generateContentForInstructionSimonov
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ScreenBroadcast());
+            //Application.Run(new ScreenBroadcast());
+            Application.Run(new Aforge());
         }
     }
 }
